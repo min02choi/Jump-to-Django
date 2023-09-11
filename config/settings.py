078af0d8074d25cef1ec507f74d9ffbd61112eb3
min02choi <min02choi@naver.com>
2023-09-12 03:23:11 +0900

    [Ch02-3] DB Model Setting

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
