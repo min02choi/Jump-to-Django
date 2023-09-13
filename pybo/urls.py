73adc8ea5a4251fa9d4c79aa842bd235ff09053a
min02choi <min02choi@naver.com>
2023-09-13 20:36:57 +0900

    [Ch02-6] URL setting, Answer

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+# 네임스페이스 지정
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create')
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,28 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from django.http import HttpResponse
-from django.shortcuts import render
+
+from pybo.models import Question
+
 
 def index(request):
-    return HttpResponse("안녕하세요~~")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+# 답변 등록
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(
+        content=request.POST.get('content'),
+        create_date=timezone.now()
+    )
+    return redirect('pybo:detail', question_id=question.id)
