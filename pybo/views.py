8f663496f3957c6b50f1af2f94a76fe971b6d930
min02choi <min02choi@naver.com>
2023-09-14 19:28:24 +0900

    [Ch02-10] Bootstrap, HTML (fin)

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.http import HttpResponse
 
+from .forms import QuestionForm, AnswerForm
 from pybo.models import Question
 
 
@@ -21,8 +22,30 @@ def detail(request, question_id):
 # 답변 등록
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(
-        content=request.POST.get('content'),
-        create_date=timezone.now()
-    )
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def question_create(request):
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)      # create_date에 값을 넣기 위해 일단은 임시저장
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:       # "GET"방식인 경우
+        form = QuestionForm()       # forms.py
+    context = {'form': form}
+
+    return render(request, 'pybo/question_form.html', context)
